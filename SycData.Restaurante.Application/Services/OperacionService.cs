56ddb8b759586e34c0d64cbaa241ab78271be1eb
kevin reyes <kevin_m_25@hotmail.com>
2019-06-24 19:21:59 -0500

    todo

---
+++ SycData.Restaurante.Application/Services/OperacionService.cs
@@ -0,0 +1,40 @@
+﻿using SycData.Restaurante.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SycData.Restaurante.Domain.ViewModels;
+using SycData.Restaurante.Infra.Persistence.Repository;
+
+namespace SycData.Restaurante.Application.Services
+{
+    public class OperacionService : IOperacionService
+
+    {
+
+
+        private UnitOfWork _UnitOfWork;
+
+
+        public OperacionService(UnitOfWork UnitOfWork)
+        {
+
+
+
+            _UnitOfWork = UnitOfWork;
+
+        }
+        public async Task<OperacionViewModel> GetOpenBox()
+        {
+
+            return await _UnitOfWork.Operacion.GetOpenBox();
+        }
+
+        public  async Task<TotalesViewModel> GetTotales(int IdOperacion)
+        {
+
+            return await _UnitOfWork.Operacion.GetTotales(IdOperacion);
+        }
+    }
+}
---
+++ SycData.Restaurante.Web.Api/Controllers/OperacionController.cs
@@ -0,0 +1,54 @@
+﻿using SycData.Restaurante.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace SycData.Restaurante.Web.Api.Controllers
+{
+    public class OperacionController : ApiController
+    {
+
+
+
+        private readonly OperacionService _OperacionService;
+
+
+        public OperacionController(OperacionService OperacionService)
+
+
+        {
+
+            _OperacionService = OperacionService;
+        }
+
+
+
+        [Authorize]
+        [HttpGet]
+        //[ResponseType(typeof(ViewModel))]
+        [Route("api/Operacion/GetOpenBox")]
+        public async Task<IHttpActionResult> GetOpenBox()
+        {
+            var Result = await _OperacionService.GetOpenBox();
+            return Ok(Result);  // Returns an OkNegotiatedContentResult
+        }
+
+
+
+        [Authorize]
+        [HttpGet]
+        //[ResponseType(typeof(ViewModel))]
+        [Route("api/Operacion/Totales")]
+        public async Task<IHttpActionResult> GetTotales(int IdOperacion)
+        {
+            var Result = await _OperacionService.GetTotales(IdOperacion);
+
+            return Ok(Result);  // Returns an OkNegotiatedContentResult
+        }
+
+    }
+}
