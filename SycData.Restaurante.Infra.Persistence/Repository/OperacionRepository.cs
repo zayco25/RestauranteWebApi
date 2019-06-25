3e11340daa0a1bcf09777bfa8569dc6d0d321485
kevijn <kevin_m_25@hotmail.com>
2019-06-25 01:09:50 -0500

    1

--- SycData.Restaurante.Infra.Persistence/Repository/OperacionRepository.cs
+++ SycData.Restaurante.Infra.Persistence/Repository/OperacionRepository.cs
@@ -35,22 +35,5 @@ public    class OperacionRepository:IOperacion
             return Result;
             
         }
-
-        public  async  Task<TotalesViewModel> GetTotales(int IdOperacion)
-        {
-
-            var Result = await _Context.RegistroPedido.Where(x => x.IdOperacion == IdOperacion && x.Estado == 2).SumAsync(x => (decimal?) x.Total);
-
-            var Descuento = await _Context.RegistroPedido.Where(x => x.IdOperacion == IdOperacion && x.Estado == 2).SumAsync(x => (  ( x.Descuento==null || x.Descuento==0 ?  0 : x.Descuento  )  + (x.PDescuento==null || x.PDescuento==0 ? 0 : ( ( x.PDescuento * x.Total )  / 100 ) )   ) );
-
-            TotalesViewModel Respuesta = new TotalesViewModel();
-
-            Respuesta.Total = (Result == null ? 0:Result) ;
-            Respuesta.Descuento = (Descuento==null?0:Descuento);
-
-
-
-            return Respuesta;
-        }
     }
 }
